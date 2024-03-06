835fc5a2fce4480b3d675b4600eaa184f849d1f6
Tal Avinari <talavinari@gmail.com>
2024-03-06 23:28:30 +0200

    todo bom

--- js/triage.js
+++ js/triage.js
@@ -1,5 +1,5 @@
 var app = angular.module("app", []);
-
+  
 app.controller("TriageController", ['$scope', '$rootScope', '$http', '$timeout', function($scope, $rootScope, $http, $timeout) {
     const ctrl = this;
     window.ctrl = this;
@@ -181,9 +181,11 @@ app.controller("TriageController", ['$scope', '$rootScope', '$http', '$timeout',
     ctrl.openSearchBar = function() {
         ctrl.dataShown = 'TRIAGE';
         ctrl.searchBarOpen = true; 
+        inputElement = document.getElementById('searchInput')
         $timeout(function() {
-            document.getElementById('searchInput').focus();
-          }, 100);
+            inputElement.focus();
+        }, 100);
+
     }
 
     ctrl.closeSearchBar = function() {
