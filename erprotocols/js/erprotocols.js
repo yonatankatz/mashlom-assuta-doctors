27697b0e6cbb1d956d03a7f51bc7e8ad6ee07651
Tal Avinari <talavinari@gmail.com>
2024-04-14 22:55:55 +0300

    hyperkalemia data

--- erprotocols/js/erprotocols.js
+++ erprotocols/js/erprotocols.js
@@ -13,6 +13,30 @@ app.controller("PhototherapyController", ['$scope', '$rootScope', '$http', '$tim
     ctrl.riskZoneObj = {};
     ctrl.statusColor = {};
     ctrl.considerTransfusion = '';
+    // ctrl.protocols = ['היפרקלמיה'];
+    kabala_siudit =  ['הפשטה מלאה', 'ביצוע סימנים חיוניים וניטור', 'אקג', 'במידה ויש שינויים באקג - דפיברילטור', 'VL2 ולקיחת מעבדה הכוללת ס"ד, כימיה וגזים'];
+    treatment = ['קלציום גלקונוט 10% 30 ml או קלציום כלוריד 10% 10 mL – מתן בפוש איטי דרך CL. ייתכן צורך במנות נוספות כחצי שעה לאחר מכן',
+            'בולוס של אינסולין 10 U ב dextrose 50% 50 ml',
+            'ונטולין 10 מ״ג באנהלציה מתמשכת',
+            'סודיום ביקרבונט Nahco3  150 בתוך    ליטר D5W',
+            'באנדקציה  של חמצת מטבולית קשה PH<7.1, בהחייאה, חסר כרוני של ביקרבונט במטופלים עם CKD',
+            'דיאליזה'
+        ];
+    decisions = ['במידה ואשפוז – מטופל סימפטומטי, לא יציב, מטופל שדורש דיאליזה לאור היפרקלמי רפרקטורית, מחלת בסיס המצריכה אשפוז',
+                ' יש צורך  לשקול אשפוז בט״נ או פנימית מוגבר לאחר ייצוב המטופל',
+                'התוויות לשחרור : מטופל יציב, היפרקלמיה  קלה , מטופל א-תסמיני וללא שינויים באקג'];
+    releaseNotes = ['מעקב מעבדתי – יש לחזור על ס״ד וכימיה חוזרת במסגרת הקופ״ח בעוד 3 ימים',
+                    'דיאטת דלת אשלגן',
+                    'מעקב רופא משפחה לצורך איזון טיפול תרופתי ומעקב אחר תרופות נפרוטוקסיות ותרופות אשר מעלות אשלגן',
+                    'חולה דיאליזה –?'];
+    ctrl.sections = [
+        {'id': 1, 'displayName': 'תלונה' },
+        {'id': 2, 'displayName': 'קבלה סיעודית', listItems: kabala_siudit },
+        {'id': 3, 'displayName': 'בירור', imageSource: 'assets/hyperkalemia-diagnose.png' },
+        {'id': 4, 'displayName': 'טיפול', listItems: treatment},
+        {'id': 5, 'displayName':  'החלטה', listItems: decisions},
+        {'id': 6, 'displayName':'המלצות בשחרור', listItems: releaseNotes}
+        ];
 
     ctrl.clearContent = function(attr) {
         ctrl[attr]  = null;
