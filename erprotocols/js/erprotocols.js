0785d0f58e08fd976d48ebc4bcbc5f9bf3e94491
Tal Avinari <talavinari@gmail.com>
2024-04-14 23:37:59 +0300

    remove redundant code

--- erprotocols/js/erprotocols.js
+++ erprotocols/js/erprotocols.js
@@ -4,16 +4,6 @@ app.controller("PhototherapyController", ['$scope', '$rootScope', '$http', '$tim
     const ctrl = this;
     window.ctrl = this;
     ctrl.dataShown = 'CALCULATOR'; // possible values: CALCULATOR, RISKS, PHOTOTHERAPY_GRAPH, GRAPH_BUTANI, EXCHANGE_TRANSFUSION_GRAPH
-    ctrl.weekOfBirth = 'above38';
-    ctrl.bilirubin;
-    ctrl.ageInHours;
-    ctrl.hasRiskFactors = false;
-    ctrl.rootDiagnose = '';
-    ctrl.distanceFromCurve = '';
-    ctrl.riskZoneObj = {};
-    ctrl.statusColor = {};
-    ctrl.considerTransfusion = '';
-    // ctrl.protocols = ['היפרקלמיה'];
     kabala_siudit =  ['הפשטה מלאה', 'ביצוע סימנים חיוניים וניטור', 'אקג', 'במידה ויש שינויים באקג - דפיברילטור', 'VL2 ולקיחת מעבדה הכוללת ס"ד, כימיה וגזים'];
     treatment = ['קלציום גלקונוט 10% 30 ml או קלציום כלוריד 10% 10 mL – מתן בפוש איטי דרך CL. ייתכן צורך במנות נוספות כחצי שעה לאחר מכן',
             'בולוס של אינסולין 10 U ב dextrose 50% 50 ml',
@@ -46,85 +36,6 @@ app.controller("PhototherapyController", ['$scope', '$rootScope', '$http', '$tim
         ctrl[attr]  = null;
     }
 
-    ctrl.selectWeekOfBirth = function(value) {
-        ctrl.weekOfBirth = value;
-        ctrl.changedValue();
-    };
-
-    ctrl.selectRiskFactor = function(value) {
-        ctrl.hasRiskFactors = value;
-        ctrl.changedValue();
-    }
-
-    ctrl.allInputsSatisfied = function() {
-        return ctrl.ageInHours && ctrl.bilirubin;
-    }
-
-    ctrl.riskZoneSatisfied = function() {
-        return Object.keys(ctrl.riskZoneObj).length > 0;
-    }
-
-    ctrl.clearValues = function() {
-        ctrl.rootDiagnose = '';
-        ctrl.distanceFromCurve = '';
-        ctrl.considerTransfusion = '';
-        ctrl.riskZoneObj = {};
-        ctrl.statusColor['background-color'] = '';
-    };
-
-    ctrl.changedValue = function() {   
-        if (!ctrl.allInputsSatisfied()) {
-            ctrl.clearValues();
-            return;
-        }
-        // if the user types a two digits numbers - its weird that we immediately shows him the value after the
-        // first digit. usually, the sign will be nagative. it doesn't feel good.
-        // so -
-        // for one digit - we just delay the display a little bit.
-        // after two digits - we can show the value immediately.
-        // that's why we have this timeoutMillis parameter.
-        var timeoutMillis =  ctrl.ageInHours <= 9 ? 800 : 20;
-        $timeout(function() {
-            // we need to check again, as during the timeout one may delete the value.
-            if (!ctrl.allInputsSatisfied()) {
-                ctrl.clearValues();
-                return;
-            }
-            const {shouldUse , delta} = shouldUsePhototherapy(ctrl.ageInHours, ctrl.bilirubin, ctrl.weekOfBirth === 'above38', ctrl.hasRiskFactors);
-            ctrl.rootDiagnose = shouldUse ? "נדרש טיפול באור" : "לא נדרש טיפול באור";
-            ctrl.distanceFromCurve = '(' + (shouldUse ? "מעל העקומה ב " : "מתחת לעקומה ב ") + delta + ")" ;
-            ctrl.considerTransfusion = gerTransfusionResult(ctrl.ageInHours, ctrl.bilirubin, ctrl.weekOfBirth === 'above38', ctrl.hasRiskFactors);
-            if (delta == 0) {
-                ctrl.distanceFromCurve = "(על קו העקומה)";
-            }
-            ctrl.statusColor['background-color'] = shouldUse ? 'red' : 'green';
-            if (ctrl.ageInHours >= 12){
-                const newRiskZoneObj = getRiskZone(ctrl.ageInHours, ctrl.bilirubin, ctrl.hasRiskFactors, shouldUse);
-                Object.assign(ctrl.riskZoneObj, newRiskZoneObj);
-            } else {
-                // Reset in case we already have data here from previous diagnose
-                ctrl.riskZoneObj = {};
-            }
-        }, timeoutMillis) ;
-    };
-
-    ctrl.resetAll = function() {
-        ctrl.bilirubin = '';
-        ctrl.ageInHours = '';    
-        ctrl.considerTransfusion = '';
-    };
-
-    ctrl.openGraph = function(graph, graphPath38, graphPath37) {
-        ctrl.dataShown = graph;
-        ctrl.imagePath38 = graphPath38;
-        ctrl.imagePath37 = graphPath37;
-    };
-
-  
-    ctrl.openRiskyConditions = function() {
-        ctrl.dataShown = 'RISKS';
-    };
-    
     ctrl.closePanel = function() {
         ctrl.dataShown = 'CALCULATOR';
     };
@@ -154,22 +65,3 @@ app.directive('selectOnClick', ['$window', function ($window) {
         }
     };
 }]);
-
-app.directive('risks', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'htmls/risks.html',
-        link: function(scope, element, attrs) {
-        }
-    };
-});
-
-app.directive('graphs', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'htmls/graphs.html',
-        link: function(scope, element, attrs) {
-            
-        }
-    };
-});
