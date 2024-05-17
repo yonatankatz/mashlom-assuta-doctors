05eaa786ded05f9b34fdd24b08826743eae79b92
Yonatan Katz <katzyonatan@gmail.com>
2024-05-17 10:16:36 +0300

    ui improvements

--- protocols/js/hyperkalemia.js
+++ protocols/js/hyperkalemia.js
@@ -4,24 +4,33 @@ app.controller("HyperkalemiaController", ['$scope', '$rootScope', '$http', '$tim
     const ctrl = this;
     window.ctrl = this;    
     ctrl.isExpanded = false;
-    kabala_siudit =  ['הפשטה מלאה', 'ביצוע סימנים חיוניים וניטור', 'אקג', 'במידה ויש שינויים באקג - דפיברילטור', 'VL2 ולקיחת מעבדה הכוללת ס"ד, כימיה וגזים'];
-    treatment = ['קלציום גלקונוט 10% 30 ml או קלציום כלוריד 10% 10 mL – מתן בפוש איטי דרך CL. ייתכן צורך במנות נוספות כחצי שעה לאחר מכן',
-            'בולוס של אינסולין 10 U ב dextrose 50% 50 ml',
-            'ונטולין 10 מ״ג באנהלציה מתמשכת',
-            'סודיום ביקרבונט Nahco3  150 בתוך    ליטר D5W',
-            'באנדקציה  של חמצת מטבולית קשה PH<7.1, בהחייאה, חסר כרוני של ביקרבונט במטופלים עם CKD',
-            'דיאליזה'
+    kabala_siudit =  [
+        {value:'הפשטה מלאה'},
+        {value:'ביצוע סימנים חיוניים וניטור'},
+        {value:'אקג'},
+        {value:'במידה ויש שינויים באקג - דפיברילטור'},
+        {value:'VL2 ולקיחת מעבדה הכוללת ס"ד, כימיה וגזים'}];
+    treatment = [
+        {value:'קלציום גלקונוט 10% 30 ml או קלציום כלוריד 10% 10 mL – מתן בפוש איטי דרך CL. ייתכן צורך במנות נוספות כחצי שעה לאחר מכן'},
+        {value:'בולוס של אינסולין 10 U ב dextrose 50% 50 ml'},
+        {value:'ונטולין 10 מ״ג באנהלציה מתמשכת'},
+        {value:'סודיום ביקרבונט Nahco3  150 בתוך    ליטר D5W'},
+        {value:'באנדקציה  של חמצת מטבולית קשה PH<7.1, בהחייאה, חסר כרוני של ביקרבונט במטופלים עם CKD'},
+        {value:'דיאליזה'}
         ];
-    decisions = ['במידה ואשפוז – מטופל סימפטומטי, לא יציב, מטופל שדורש דיאליזה לאור היפרקלמי רפרקטורית, מחלת בסיס המצריכה אשפוז',
-                ' יש צורך  לשקול אשפוז בט״נ או פנימית מוגבר לאחר ייצוב המטופל',
-                'התוויות לשחרור : מטופל יציב, היפרקלמיה  קלה , מטופל א-תסמיני וללא שינויים באקג'];
-    releaseNotes = ['מעקב מעבדתי – יש לחזור על ס״ד וכימיה חוזרת במסגרת הקופ״ח בעוד 3 ימים',
-                    'דיאטת דלת אשלגן',
-                    'מעקב רופא משפחה לצורך איזון טיפול תרופתי ומעקב אחר תרופות נפרוטוקסיות ותרופות אשר מעלות אשלגן',
-                    'חולה דיאליזה –?'];
-    onGoingTreatment = ['אקג חוזר לאחר טיפול ',
-                        'גזים חוזרים פעם בשעה',
-                        'קטטר במידת הצורך'];
+    decisions = [
+        {value:'במידה ואשפוז – מטופל סימפטומטי, לא יציב, מטופל שדורש דיאליזה לאור היפרקלמי רפרקטורית, מחלת בסיס המצריכה אשפוז'},
+        {value:' יש צורך  לשקול אשפוז בט״נ או פנימית מוגבר לאחר ייצוב המטופל'},
+        {value:'התוויות לשחרור : מטופל יציב, היפרקלמיה  קלה , מטופל א-תסמיני וללא שינויים באקג'}];
+    releaseNotes = [
+        {value:'מעקב מעבדתי – יש לחזור על ס״ד וכימיה חוזרת במסגרת הקופ״ח בעוד 3 ימים'},
+        {value:'דיאטת דלת אשלגן'},
+        {value:'מעקב רופא משפחה לצורך איזון טיפול תרופתי ומעקב אחר תרופות נפרוטוקסיות ותרופות אשר מעלות אשלגן'},
+        {value:'חולה דיאליזה –?'}];
+    onGoingTreatment = [
+        {value:'אקג חוזר לאחר טיפול '},
+        {value:'גזים חוזרים פעם בשעה'},
+        {value:'קטטר במידת הצורך'}];
     caseTypes = ['קל: 5.5-6 mEq/L',
     'בנוני: 6.1-6.9 mEq/L',
     'קשה: 6.9-7 mEq/L'];
@@ -38,15 +47,17 @@ app.controller("HyperkalemiaController", ['$scope', '$rootScope', '$http', '$tim
         ];
 
     ctrl.toggleCollabse = function() {
-        ctrl.isExpanded = !ctrl.isExpanded;            
-        var accordionButtons = document.querySelectorAll('.accordion-button');
-        accordionButtons.forEach(function(button) {                
-            if (button.getAttribute("aria-expanded") == 'true' && !ctrl.isExpanded) {
-                button.click();                    
-            }
-            else if (button.getAttribute("aria-expanded") == 'false' && ctrl.isExpanded) {
-                button.click(); 
-            }                                
+        $timeout(function(){
+            ctrl.isExpanded = !ctrl.isExpanded;            
+            var accordionButtons = document.querySelectorAll('.accordion-button');
+            accordionButtons.forEach(function(button) {                
+                if (button.getAttribute("aria-expanded") == 'true' && !ctrl.isExpanded) {
+                    button.click();                    
+                }
+                else if (button.getAttribute("aria-expanded") == 'false' && ctrl.isExpanded) {
+                    button.click(); 
+                }                                
+            });    
         });
     };
 
