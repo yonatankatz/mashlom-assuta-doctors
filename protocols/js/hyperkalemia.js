a4b150c9ea0974a1bc9a7f51fec5d24e0faf2bac
Yonatan Katz <katzyonatan@gmail.com>
2024-05-19 23:24:59 +0300

    fadiha!!

--- protocols/js/hyperkalemia.js
+++ protocols/js/hyperkalemia.js
@@ -1,4 +1,4 @@
-var app = angular.module("app", []);
+var app = angular.module("app", ['ngSanitize']);
   
 app.controller("HyperkalemiaController", ['$scope', '$rootScope', '$http', '$timeout', function($scope, $rootScope, $http, $timeout) {
     const ctrl = this;
