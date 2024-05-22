ff5fa34135a929e74593ea9f6b59f2f98159e26c
Yonatan Katz <katzyonatan@gmail.com>
2024-05-22 21:16:07 +0300

    fix back botton

--- protocols/js/protocols.js
+++ protocols/js/protocols.js
@@ -1,6 +1,6 @@
 var app = angular.module("app", []);
-  
-app.controller("ProtocolsController", ['$scope', '$rootScope', '$http', '$timeout', function($scope, $rootScope, $http, $timeout) {
+
+app.controller("ProtocolsController", ['$scope', '$rootScope', '$http', '$timeout', function($scope, $rootScope, $http, $timeout) {    
     const ctrl = this;
     window.ctrl = this;
     ctrl.openedSection = -1;
@@ -9,7 +9,7 @@ app.controller("ProtocolsController", ['$scope', '$rootScope', '$http', '$timeou
     ctrl.urlSuffix = window.location.href.indexOf("localhost") > -1 ||
                      window.location.href.indexOf("127.0.0.1") > -1 ? ".html" : "";
 
-    function init() {
+    function init() {        
         $http.get('./data/protocols.json').then(function(response) {
             ctrl.protocols = response.data.protocols;
         })
@@ -39,3 +39,21 @@ app.controller("ProtocolsController", ['$scope', '$rootScope', '$http', '$timeou
     
 
 }]);
+
+// collapse all accordions. the bug that it fixes is this:
+// open an accordion, press on one of the sub-links, then press "back"
+// in the browser.
+// the accordion is still open.
+window.addEventListener('pageshow', function(event) {
+    if (event.persisted) {
+        var accordions = document.querySelectorAll('.accordion .collapse');
+        accordions.forEach(function(accordion) {
+            accordion.classList.remove('show');
+        });
+        var openedElements = document.querySelectorAll('.opened-accordion-color');
+        openedElements.forEach(function(element) {
+            element.classList.remove('opened-accordion-color');
+            element.classList.add('closed-accordion-color');
+        });
+    }
+});
