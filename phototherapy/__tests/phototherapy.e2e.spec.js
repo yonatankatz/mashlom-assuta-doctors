b622da6a055c65889a754c6bb8838aa59c802ee9
Daniel Kagan <danielk@wix.com>
2024-06-15 18:35:05 +0300

    Add blood trafusion e2e to phototherapy

--- phototherapy/__tests/phototherapy.e2e.spec.js
+++ phototherapy/__tests/phototherapy.e2e.spec.js
@@ -6,12 +6,19 @@ const LIGHT_TREATMENT = {
   NEED_CARE: 'נדרש טיפול באור'
 }
 
-const TRANSFUSION_RESULT = {
-  A: 'עובר את סף החלפת דם (בילירובין גדול מגיל הילד)',
-  B: 'עובר את סף החלפת דם',
-  C: 'ערך בילירובין מתקרב לסף החלפת דם, יש לשקול מתן IVIG',
-  D: 'ערך בילירובין מתקרב לסף החלפת דם',
+const BLOOD_TRANSFUSION = {
+  ALERT: 'ערך בילירובין מתקרב לסף החלפת דם',
+  NEED: 'עובר את סף החלפת דם',
+  IVIG: 'ערך בילירובין מתקרב לסף החלפת דם, יש לשקול מתן IVIG'
 }
+
+const transfusionNameToId = {
+  [BLOOD_TRANSFUSION.ALERT]: 'התראה',
+  [BLOOD_TRANSFUSION.NEED]: 'עובר',
+  [BLOOD_TRANSFUSION.IVIG]: 'התראה IVIG'
+}
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const getPhototherapyResult = async (isAbove38, isRisky, bilirobinValue, ageInHours) => {
   const browser = await puppeteer.launch({headless: true, devtools: true });
@@ -44,23 +51,25 @@ const getPhototherapyResult = async (isAbove38, isRisky, bilirobinValue, ageInHo
   // Hack, but we need to wait for the result to be fully rendered.
   // After moving to react we can move remove the sleep.
   if (ageInHours <=9){
-    await new Promise(r => setTimeout(r, 900));
+    await sleep(900);
   }else{
-    await new Promise(r => setTimeout(r, 100));
+    await sleep(100);
   }
 
   // Get the result and return it
   const rootDiagnose = await page.$eval('#root-diagnose', el => el.textContent);
   const shouldFollowUp = await page.$eval('#should-followup', el => el.clientHeight > 0);
   const _riskZone = await page.$eval('#risk-zone', el => el.textContent);
-  const transfusionResult = await page.$eval('#transfusion-result', el => el.textContent);
+  const _transfusionResult = await page.$eval('#transfusion-result', el => el.textContent);
   
   const needLightTreatment = rootDiagnose.trim() === LIGHT_TREATMENT.NO_NEED_CARE ? false : true;
   // check if _riskZone is a number, if so, parse it to int, else parse it to 0
   const riskZone = isNaN(parseInt(_riskZone)) ? 0 : parseInt(_riskZone);
+  const transfusionResult = transfusionNameToId[_transfusionResult.trim()] ?? 'ריק';
+  
   await browser.close();
 
-  return { needLightTreatment, riskZone, shouldFollowUp };
+  return { needLightTreatment, riskZone, transfusionResult, shouldFollowUp };
 }
 
 // should remove
@@ -71,9 +80,10 @@ describe('Phototherapy-e2e', () => {
   it.each(testCases)(
     "test case %o",
     async ({above38, hasRisk, ageInHours, bilirubin, expectedResult}) => {
-      const {needLightTreatment, riskZone, shouldFollowUp} = await getPhototherapyResult(above38, hasRisk, bilirubin, ageInHours);
+      const {needLightTreatment, riskZone, transfusionResult} = await getPhototherapyResult(above38, hasRisk, bilirubin, ageInHours);
       expect(needLightTreatment).toEqual(expectedResult.needLightTreatment)
       expect(riskZone).toEqual(expectedResult.riskZone)
+      expect(transfusionResult).toEqual(expectedResult.transfusionResult)
       // expect(shouldFollowUp).toEqual(false)
     }
   );
