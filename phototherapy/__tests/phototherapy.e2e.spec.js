166b58ceb40c62430a5828410acd6b157ef63bfd
Tal Avinari <talavinari@gmail.com>
2024-06-26 13:12:53 +0300

    enhance e2e with explanation

--- phototherapy/__tests/phototherapy.e2e.spec.js
+++ phototherapy/__tests/phototherapy.e2e.spec.js
@@ -20,6 +20,22 @@ const transfusionNameToId = {
   [BLOOD_TRANSFUSION.NEED_UNDER_SIX]: 'עובר גדול מגיל ילוד'
 }
 
+const TREATMENT_EXPLANATION = {
+  LIGHT_TREATMENT: 'טיפול אור באשפוז',
+  SERUM_8_HOURS: 'המשך אשפוז עם מעקב בילירובין בסרום כל 8 שעות',
+  FOLLOW_UP_24_HOURS: 'שחרור עם מעקב בילירובין חוזר תוך 24 שעות',
+  FOLLOW_UP_48_HOURS: 'שחרור עם המלצה למעקב בילירובין חוזר, מלעורי או בסרום, תוך 48 שעות',
+  COMMUNITY_FOLLOW_UP: 'שחרור למעקב שגרתי בקהילה, רופא מטפל וטיפת חלב'
+}
+
+const explanationNameToId = {
+  [TREATMENT_EXPLANATION.LIGHT_TREATMENT]: 'אור',
+  [TREATMENT_EXPLANATION.SERUM_8_HOURS]: 'סרום 8',
+  [TREATMENT_EXPLANATION.FOLLOW_UP_24_HOURS]: 'מעקב 24',
+  [TREATMENT_EXPLANATION.FOLLOW_UP_48_HOURS]: 'מעקב 48',
+  [TREATMENT_EXPLANATION.COMMUNITY_FOLLOW_UP]: 'קהילה'
+}
+
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const getPhototherapyResult = async (isAbove38, isRisky, bilirobinValue, ageInHours) => {
@@ -63,15 +79,18 @@ const getPhototherapyResult = async (isAbove38, isRisky, bilirobinValue, ageInHo
   const shouldFollowUp = await page.$eval('#should-followup', el => el.clientHeight > 0);
   const _riskZone = await page.$eval('#risk-zone', el => el.textContent);
   const _transfusionResult = await page.$eval('#transfusion-result', el => el.textContent);
+  const _explanationResult = await page.$eval('#explanation', el => el.textContent);
   
   const needLightTreatment = rootDiagnose.trim() === LIGHT_TREATMENT.NO_NEED_CARE ? false : true;
   // check if _riskZone is a number, if so, parse it to int, else parse it to 0
   const riskZone = isNaN(parseInt(_riskZone)) ? 0 : parseInt(_riskZone);
   const transfusionResult = transfusionNameToId[_transfusionResult.trim()] ?? 'ריק';
+  const explanationResult = explanationNameToId[_explanationResult.trim()] ?? 'ריק';
+
   
   await browser.close();
 
-  return { needLightTreatment, riskZone, transfusionResult, shouldFollowUp };
+  return { needLightTreatment, riskZone, transfusionResult, shouldFollowUp, explanationResult };
 }
 
 // should remove
@@ -82,10 +101,11 @@ describe('Phototherapy-e2e', () => {
   it.each(testCases)(
     "test case %o",
     async ({above38, hasRisk, ageInHours, bilirubin, expectedResult}) => {
-      const {needLightTreatment, riskZone, transfusionResult} = await getPhototherapyResult(above38, hasRisk, bilirubin, ageInHours);
+      const {needLightTreatment, riskZone, transfusionResult, explanationResult} = await getPhototherapyResult(above38, hasRisk, bilirubin, ageInHours);
       expect(needLightTreatment).toEqual(expectedResult.needLightTreatment)
       expect(riskZone).toEqual(expectedResult.riskZone)
       expect(transfusionResult).toEqual(expectedResult.transfusionResult)
+      expect(explanationResult).toEqual(expectedResult.explanationResult)
       // expect(shouldFollowUp).toEqual(false)
     }
   );
